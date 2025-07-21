8a1322c31818fc1583ea6870208bc833f90ba8a3
Yasin Atıcı <yasinatici273@gmail.com>
2025-07-21 12:16:38 +0300

    created extension ui

---
+++ src/components/extensions-cards.tsx
@@ -0,0 +1,7 @@
+export default function ExtensionCards({children}: {children?: React.ReactNode}) {
+  return (
+    <section>
+        <div className="container">{children}</div>
+    </section>
+  )
+}
